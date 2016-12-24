8e5f382094fa92a0501e9145e4f52e41f42862aa
pshingavi <preetams3@gmail.com>
2016-12-23 20:09:07 -0800

    Data Binding: String interpolation

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -4,17 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <app-other></app-other>
-    <app-another>
-      <div>
-        <h1>Hello</h1>  <!--Style for this element is used from the current component and not from app-another-->
-        <p>This is external code</p>
-      </div>
-    </app-another>
-    <app-another>
-      <p>Something more</p>
-    </app-another>
-    <app-another></app-another>
+    <app-databinding></app-databinding>
   `,
   //templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { ManualComponent } from './manual/manual.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
-import { AnotherComponent } from './other/another.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OtherComponent,
-    ManualComponent,
-    AnotherComponent
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
