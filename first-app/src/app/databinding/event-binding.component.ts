ff7a29ab015e531dceffab876e5dbe0c28827dd8
pshingavi <preetams3@gmail.com>
2016-12-24 14:22:16 -0800

    Event binding - Native event (click)

---
+++ first-app/src/app/databinding/event-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  template: `
+    <button (click)="onClicked()">Click me</button>
+  `,
+  styles: []
+})
+export class EventBindingComponent {
+  onClicked() {
+    alert('Clicked Me!');
+  }
+}
