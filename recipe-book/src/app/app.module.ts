647574930bd3ffc92eefac28c0c5920ea0fe3b38
pshingavi <preetams3@gmail.com>
2016-12-27 15:32:20 -0800

    Add header component with minimal setup

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { RecipeBookComponent } from './recipe-book.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
-    RecipeBookComponent
+    RecipeBookComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
