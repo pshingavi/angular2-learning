c29e299ae75012ba1e1f376cff2410af3900ae8e
pshingavi <preetams3@gmail.com>
2016-12-29 15:24:06 -0800

    Configure routing, register to angular2 RouterModule and export as module to imports in AppModule

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -1,10 +1,10 @@
+import { ConfiguredRouting } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HomeComponent } from './home.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user/user-detail.component';
@@ -21,8 +21,7 @@ import { UserEditComponent } from './user/user-edit.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    ConfiguredRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ routing/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home.component';
+import {Routes, RouterModule} from "@angular/router"
+// Configure the routes and export to the AppModule
+const APP_ROUTES: Routes = [
+  // Each route is a js object
+  // First let's have route for home Component
+  { path: '', component: HomeComponent },  // localhost:4200
+  // User component
+  { path: 'user', component: UserComponent}  // localhost:4200/user
+];
+
+// Register the routes to the RouterModule and export to the root
+export const ConfiguredRouting = RouterModule.forRoot(APP_ROUTES);
