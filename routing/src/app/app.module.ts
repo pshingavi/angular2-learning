1eb8035d7bffa343257720b1952c2ce7c5fdf216
pshingavi <preetams3@gmail.com>
2016-12-29 15:04:01 -0800

    Routing module startup project

---
+++ routing/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user/user-detail.component';
+import { UserEditComponent } from './user/user-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HomeComponent,
+    UserComponent,
+    UserDetailComponent,
+    UserEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
