02c0b032323e0a93d2bd673967d0ed8d7828f86c
pshingavi <preetams3@gmail.com>
2016-12-29 17:18:59 -0800

    Extracting query param with queryParams.subscribe on ActivatedRoute

--- routing/src/app/home.component.ts
+++ routing/src/app/home.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
-    <p>
-      home Works!
-    </p>
+    <h2>Home Component !</h2>
+    <hr>
+    {{analytics}}
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  analytics: string;
 
-  constructor() { }
+  private subscription: Subscription;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.subscription = activatedRoute.queryParams.subscribe(
+      queryParam => this.analytics = queryParam['analytics']
+    );
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    // Prevent memory leak
+    this.subscription.unsubscribe();
   }
 
 }
