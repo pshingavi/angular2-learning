af57b6a54c995819167116587243f98d365d5e13
pshingavi <preetams3@gmail.com>
2016-12-30 15:10:45 -0800

    Redirecting routes

--- routing/src/app/app.routing.ts
+++ routing/src/app/app.routing.ts
@@ -5,10 +5,14 @@ import {Routes, RouterModule} from "@angular/router"
 // Configure the routes and export to the AppModule
 const APP_ROUTES: Routes = [
   // Each route is a js object
+  // Redirect
+  { path:'user', redirectTo: '/user/1', pathMatch: 'full' },
   // First let's have route for home Component
   { path: '', component: HomeComponent },  // localhost:4200
   // User component
-  { path: 'user/:id', component: UserComponent, children: USER_ROUTES}  // localhost:4200/user/1
+  { path: 'user/:id', component: UserComponent, children: USER_ROUTES},  // localhost:4200/user/1
+  // If route doesn't exist, redirect to default route
+  { path:'**', redirectTo: '/user/1', pathMatch: 'full' },
 ];
 
 // Register the routes to the RouterModule and export to the root
