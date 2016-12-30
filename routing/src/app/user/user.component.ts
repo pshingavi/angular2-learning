b5ae44ffd34783646108177a2d39725669bdb15b
pshingavi <preetams3@gmail.com>
2016-12-29 16:00:55 -0800

    Imperative routing

--- routing/src/app/user/user.component.ts
+++ routing/src/app/user/user.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user',
   template: `
     <h1>user Component</h1>
-    <a [routerLink]="['user']">User</a>
-    <a [routerLink]="['../']">Home</a>
+    <a [routerLink]="['/user']">User</a>
+    <h2>Imperative Routing</h2>
+    <button (click)="onNavigate()">Go Home</button>
   `,
   styles: []
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onNavigate() {
+    this.router.navigate(['/']);
   }
 
 }
