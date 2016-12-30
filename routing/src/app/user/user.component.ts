022ec964d8b3f69c9620fa4237cc72d43777f2a6
pshingavi <preetams3@gmail.com>
2016-12-29 16:53:48 -0800

    Reactive behaviour to params, Obesevable params of avtivatedRoute

--- routing/src/app/user/user.component.ts
+++ routing/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs/Rx"
 
 @Component({
   selector: 'app-user',
@@ -12,10 +13,14 @@ import {Router, ActivatedRoute} from "@angular/router";
   `,
   styles: []
 })
-export class UserComponent implements OnInit {
-  private id: string;
+export class UserComponent implements OnInit, OnDestroy {
+  id: string;
+  private subscription: Subscription;  // Need this to destroy the subscription in the constructor which by default is not destroyed when the component is destroyed. Causes memory leak
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-    this.id = activatedRoute.snapshot.params['id'];
+    //this.id = activatedRoute.snapshot.params['id'];
+    this.subscription = activatedRoute.params.subscribe(
+      (param: any) => this.id = param["id"]  // Call back function (first of the 3 callbacks)
+    );
   }
 
   ngOnInit() {
@@ -23,6 +28,10 @@ export class UserComponent implements OnInit {
 
   onNavigate() {
     this.router.navigate(['/']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();   // Prevent memory leaks. Destroy subscription mentioned in constructor
   }
 
 }
