67a5c490d3a8b9a4abc6cfe71eccbc2a8806aa11
pshingavi <preetams3@gmail.com>
2017-01-06 12:13:52 -0800

    Project - Recipe form - Preparation - Fetching active recipe, parsing index or id of the recipe via subscription to ActiveRoute params changes

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styles: []
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private recipeIndex: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
+    // Extract the route params. Can be done in the constructor, but leave the heavy lifting here
+    // Let's subscribe to the params in the Route, also unsubscribe in ngOnDestroy
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        // I want to update the index or id of the recipe
+        if(params.hasOwnProperty('id')) {
+          // In edit existing recipe mode
+          this.recipeIndex = params['id'];  // using this statement alone will fail for /recipes/new since id is not in the route.
+        } else {
+          // In add new recipe mode
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // Avoid memory leaks because of the subscription to route params
+    this.subscription.unsubscribe();
   }
 
 }
