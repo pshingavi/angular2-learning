fb17578fc0104b2bdb0be8380227541a8a10df27
pshingavi <preetams3@gmail.com>
2017-01-05 17:31:30 -0800

    Project - Add ingredient to shopping list using form (ngSubmit)

--- recipe-book/src/app/shopping-list/shopping-list-add.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list-add.component.ts
@@ -1,3 +1,5 @@
+import { ShoppingListService } from './shopping-list.service';
+import { Ingredient } from '../shared/ingredient';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  item: Ingredient;
   isAdd: boolean = true;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  onSubmit(ingredient: Ingredient) {
+    console.log(ingredient);
+    // Add the ingredient to shopping list
+    if(!this.isAdd) {
+      // Edit
+    } else {
+      // Add
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.shoppingListService.addItem(this.item);
+    }
+  }
 }
