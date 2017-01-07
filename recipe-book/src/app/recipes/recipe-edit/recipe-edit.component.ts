00b562b1fcfcf1f94463a60cff8fd94721ac5137
pshingavi <preetams3@gmail.com>
2017-01-06 17:44:52 -0800

    Project - Sync HTML with form data - [formGroup], formControlName and formArrayName

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe';
-import { FormArray, FormControl, FormGroup, Validator, Validators, FormBuilder } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Subscription } from 'rxjs/Rx';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute } from '@angular/router';
@@ -14,7 +14,7 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
   private recipeIndex: number;
   private recipe: Recipe;
-  private isNew: boolean;
+  private isNew: boolean = true;
   private recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
@@ -29,15 +29,17 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
         // I want to update the index or id of the recipe
         if(params.hasOwnProperty('id')) {
           // In edit existing recipe mode
-          this.recipeIndex = params['id'];  // using this statement alone will fail for /recipes/new since id is not in the route.
+          this.recipeIndex = +params['id'];  // using this statement alone will fail for /recipes/new since id is not in the route. + prefix converts string to number
           this.recipe = this.recipeService.getRecipe(this.recipeIndex);
           this.isNew = false;
         } else {
           // In add new recipe mode
           this.isNew = true;
+          this.recipe = null;
         }
       }
     );
+    this.initForm();
   }
 
   ngOnDestroy() {
@@ -51,12 +53,13 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     let recipeContent = '';
     let recipeIngredients: FormArray = new FormArray([]);
     // If we are in edit mode then create the FormControls for each ingredient
+    console.log(this.isNew);
     if(!this.isNew) {
       for(let i=0; i<this.recipe.ingredients.length; i++) {
         recipeIngredients.push(
           new FormGroup({
             name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
-            amount: new FormControl(this.recipe.ingredients[i].amount, Validators.required, Validators.pattern("\\d+"))
+            amount: new FormControl(this.recipe.ingredients[i].amount, [Validators.required, Validators.pattern("\\d+")])
           })
         );
       }
