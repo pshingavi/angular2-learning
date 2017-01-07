0a064653c91255360f495f172d920a651da9309f
pshingavi <preetams3@gmail.com>
2017-01-06 16:56:00 -0800

    Project - Create the recipe form - data driven approach

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,3 +1,5 @@
+import { Recipe } from './../recipe';
+import { FormArray, FormControl, FormGroup, Validator, Validators, FormBuilder } from '@angular/forms';
 import { Subscription } from 'rxjs/Rx';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute } from '@angular/router';
@@ -11,9 +13,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class RecipeEditComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
   private recipeIndex: number;
+  private recipe: Recipe;
+  private isNew: boolean;
+  private recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-              private recipeService: RecipeService) { }
+              private recipeService: RecipeService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     // Extract the route params. Can be done in the constructor, but leave the heavy lifting here
@@ -24,8 +30,11 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
         if(params.hasOwnProperty('id')) {
           // In edit existing recipe mode
           this.recipeIndex = params['id'];  // using this statement alone will fail for /recipes/new since id is not in the route.
+          this.recipe = this.recipeService.getRecipe(this.recipeIndex);
+          this.isNew = false;
         } else {
           // In add new recipe mode
+          this.isNew = true;
         }
       }
     );
@@ -34,6 +43,36 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     // Avoid memory leaks because of the subscription to route params
     this.subscription.unsubscribe();
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImageURL = '';
+    let recipeContent = '';
+    let recipeIngredients: FormArray = new FormArray([]);
+    // If we are in edit mode then create the FormControls for each ingredient
+    if(!this.isNew) {
+      for(let i=0; i<this.recipe.ingredients.length; i++) {
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+            amount: new FormControl(this.recipe.ingredients[i].amount, Validators.required, Validators.pattern("\\d+"))
+          })
+        );
+      }
+      recipeName = this.recipe.name;
+      recipeImageURL = this.recipe.imagePath;
+      recipeContent = this.recipe.description;
+    }
+
+    // Create a form for new or edit mode
+    this.recipeForm = this.formBuilder.group({
+      name: [recipeName, Validators.required],
+      imagePath: [recipeImageURL, Validators.required],
+      description: [recipeContent, Validators.required],
+      ingredients: recipeIngredients
+    });
+
   }
 
 }
