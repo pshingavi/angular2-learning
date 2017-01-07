87ca00eec2afd6b38aa408f2f6693209c4ece2a2
pshingavi <preetams3@gmail.com>
2017-01-06 16:57:55 -0800

    Project - Add ReactiveFormsModule in AppModule imports list - data driven approach

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { RecipeBookComponent } from './recipe-book.component';
@@ -35,7 +35,8 @@ import { RecipeStartComponent } from './recipes/recipe-start.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    ConfiguredRouting
+    ConfiguredRouting,
+    ReactiveFormsModule
   ],
   providers: [ShoppingListService],
   bootstrap: [RecipeBookComponent]
