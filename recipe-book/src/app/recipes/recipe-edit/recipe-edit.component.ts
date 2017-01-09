1e9b0ec78818c1d765e94a0094b118463500e28f
pshingavi <preetams3@gmail.com>
2017-01-09 14:51:50 -0800

    Add / Edit recipe ingredients

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -93,6 +93,19 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     this.navigateBack();
   }
 
+  onAddItem(name: String, amount: number) {
+    (<FormArray>this.recipeForm.controls['ingredients']).push(
+      new FormGroup({
+        name: new FormControl(name, Validators.required),
+        amount: new FormControl(amount, [Validators.required, Validators.pattern("\\d+")])
+      })
+    );
+  }
+
+  onRemoveItem(index: number) {
+    (<FormArray>this.recipeForm.controls['ingredients']).removeAt(index);
+  }
+
   private navigateBack() {
     // Navigate back to starting position. Needs router
     this.router.navigate(['../']);
