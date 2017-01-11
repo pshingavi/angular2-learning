f30985b8d952ee2d093998858001efa63edd49be
pshingavi <preetams3@gmail.com>
2017-01-10 18:03:58 -0800

    Async Impure pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { setTimeout } from 'timers';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -9,4 +10,11 @@ export class AppComponent {
   myValue = 'lowercase';
   myDate = new Date(2016,5,24);
   values = ["Milk", "Beans", "Bread"];
+  asyncValues = new Promise(
+    (resolve, reject) => {
+      setTimeout(
+        () => resolve('Data is here!'), 2000
+      );
+    }
+  );
 }
