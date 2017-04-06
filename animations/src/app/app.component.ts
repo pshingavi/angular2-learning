3d190378c802e4b0ff66740ae788cb90f4d296fb
pshingavi <preetams3@gmail.com>
2017-04-06 13:32:38 -0700

    The void state

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -52,7 +52,29 @@ import { animate, Component, state, style, trigger, transition } from '@angular/
         // Before final state transformation
         animate(500)  // Now transition to end state
       ])
-    ])
+    ]),
+    trigger('list1', [
+      // Animation meta data - for our state value below
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      // Animate from not-existent to any state. This is when new element enters the dom
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+        ]),
+        // Animate to delete
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+        ])
+    ]),
   ]
 })
 export class AppComponent {
