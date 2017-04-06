9537575d1b7a2923d5d9078e8ae6b8d69ce75dc0
pshingavi <preetams3@gmail.com>
2017-04-06 12:28:17 -0700

    Advanced transitions

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -15,13 +15,34 @@ import { animate, Component, state, style, trigger, transition } from '@angular/
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      //Both in one definition
+      //transition('normal <=> highlighted', animate(300)),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      // Animation meta data - for our state value below
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(200)),
+      transition('highlighted => normal', animate(1200)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item: any) {
@@ -34,5 +55,10 @@ export class AppComponent {
 
   onAnimate() {
     this.state = (this.state == 'normal') ? 'highlighted' : 'normal';
+    this.wildState = (this.wildState == 'normal') ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 }
