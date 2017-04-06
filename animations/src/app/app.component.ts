568a56ed7be096b666daf858d6efef27e74eb4e7
pshingavi <preetams3@gmail.com>
2017-04-06 12:07:37 -0700

    Animations starter

---
+++ animations/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, state, style, trigger } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('divState', [
+      // Animation meta data - for our state value below
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
+})
+export class AppComponent {
+  state = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item: any) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+}
